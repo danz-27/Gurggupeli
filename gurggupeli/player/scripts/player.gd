ab4bf5de761995dc3a86c6b6b812e8daeac65408
Daniel <peltomaad@gmail.com>
2025-07-25 03:07:06 +0300

    a doublejump that maybe works

--- gurggupeli/player/scripts/player.gd
+++ gurggupeli/player/scripts/player.gd
@@ -2,25 +2,32 @@ extends CharacterBody2D
 
 var speed := 32
 var jump_speed := -400.0
-
+var jump_amount := 1
 
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready() -> void:
 	pass
 	
-	
-func _physics_process(delta: float) -> void:
-	
-	velocity.y += gravity / 60
-		
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_speed
 
+func _physics_process(_delta: float) -> void:
+	
+	if not is_on_floor():
+		velocity.y += gravity / 60
+		
+	#jump & doublejump
+	if Input.is_action_just_pressed("jump") and jump_amount > 0:
+		jump_amount -= 1
+		velocity.y = jump_speed
+		
+	if is_on_floor() and jump_amount != 1:
+		jump_amount = 1
+	print(velocity.y)
+	#movement left & right
 	var direction := Input.get_axis("move_left", "move_right")
 	velocity.x = direction * speed
 	
-	print(velocity)
+	#print(velocity)
 	
 	set_player_flip_h()
 	animate_player()
@@ -30,12 +37,20 @@ func _physics_process(delta: float) -> void:
 
 func animate_player() -> void:
 	
+	#walk right & left
 	if velocity.x != 0:
 		$AnimationPlayer.play("walk")
 
 	elif not ($AnimationPlayer.is_playing() and $AnimationPlayer.current_animation == "walk"):
 		$AnimationPlayer.play("idle")
-
+	
+	#jump & fall
+	if velocity.y > 0:
+		$AnimationPlayer.play("falling")
+	if velocity.y < 0:
+		$AnimationPlayer.play("falling_up")
+		
+	
 
 func set_player_flip_h() -> void:
 	
