1e563e8663307cef95b765fb485a6f8b461328a2
Daniel <peltomaad@gmail.com>
2025-08-07 21:05:17 +0300

    kosmo halus dashikoodin

--- gurggupeli/player/scripts/player.gd
+++ gurggupeli/player/scripts/player.gd
@@ -1,39 +1,86 @@
 extends CharacterBody2D
 
-var speed := 32
+var move_speed := 128
 var jump_speed := -400.0
-var jump_amount := 1
-
-var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+var dash_speed := 256
+var gravity := 1000
+var dash_count := 1
+@onready var dash_duration := $Timer
 
 func _ready() -> void:
 	pass
-	
+
 
 func _physics_process(_delta: float) -> void:
 	
-	if not is_on_floor():
-		velocity.y += gravity / 60
-		
-	#jump & doublejump
-	if Input.is_action_just_pressed("jump") and jump_amount > 0:
-		jump_amount -= 1
+	
+	
+	if Input.is_action_just_pressed("jump") && is_on_floor():
 		velocity.y = jump_speed
-		
-	if is_on_floor() and jump_amount != 1:
-		jump_amount = 1
-	print(velocity.y)
-	#movement left & right
-	var direction := Input.get_axis("move_left", "move_right")
-	velocity.x = direction * speed
 	
-	#print(velocity)
 	
+	if is_on_floor():
+		dash_count = 1
+	else:
+		velocity.y += gravity / 60
+	
+	velocity.x *= 0.75
+	if velocity.x < 0.1:
+		velocity.x = 0
+	
+	move_player()
 	set_player_flip_h()
 	animate_player()
+	print("velocity ", velocity)
+	print("gravity ", gravity)
 	move_and_slide()
+
+#varied jump height
+func _input(event):
+	if event.is_action_released("jump"):
+		if velocity.y < 0.0:
+			velocity.y *= 0.5
+
+func move_player() -> void:
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
+
+	if (direction == Vector2.RIGHT || direction == Vector2.LEFT):
+		check_dash(direction)
+		velocity.x = direction.x * move_speed
+		
+	if direction.x > 0 && direction.y < 0: #UPRIGHT
+		check_dash(direction)
+		velocity.x = move_speed
+		
+	if direction.x < 0 && direction.y < 0: #UPLEFT
+		check_dash(direction)
+		velocity.x = -move_speed
+	
+	if direction.x > 0 && direction.y > 0: #DOWNRIGHT
+		check_dash(direction)
+		velocity.x = move_speed
 	
 	
+	
+		
+		
+func check_dash(dash_direction: Vector2) -> void:
+	var fixed_dash_direction = dash_direction
+	if !dash_duration.is_stopped():
+		velocity = fixed_dash_direction * dash_speed
+		return
+		
+	if dash_duration.is_stopped():
+		gravity = 1000
+		
+	if Input.is_action_just_pressed("dash") && dash_count > 0:
+		dash_count -= 1
+		velocity = Vector2.ZERO
+		dash_duration.start(0.4)
+		velocity = dash_direction * dash_speed
+
+
+
 
 func animate_player() -> void:
 	
@@ -57,6 +104,3 @@ func set_player_flip_h() -> void:
 	if velocity.x != 0:
 		$Gurggu.flip_h = velocity.x > 0
 	
-
-
- 
