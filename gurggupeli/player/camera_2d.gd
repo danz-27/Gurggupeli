6d328951898d7006095cfcae36fb740dacf93688
Daniel <peltomaad@gmail.com>
2025-10-22 15:31:07 +0300

    environment assettei

--- gurggupeli/player/camera_2d.gd
+++ gurggupeli/player/camera_2d.gd
@@ -1,7 +1,11 @@
 extends Camera2D
+class_name Camera
+
+static var instance: Camera
 
 var scale_width : int = ProjectSettings.get_setting("display/window/size/viewport_width") / 320
 var scale_height : int = ProjectSettings.get_setting("display/window/size/viewport_height") / 180
 
 func _ready() -> void:
+	instance = self
 	set_zoom(Vector2(scale_width,scale_height))
