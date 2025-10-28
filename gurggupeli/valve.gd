bec96bac0a0667f4ef760e7c2294a45f94789f2e
Kosmo <kosmo.konttila@gmail.com>
2025-10-28 14:27:04 +0200

    oon legit putkimies

--- gurggupeli/valve.gd
+++ gurggupeli/valve.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 var valve_children : Array = []
+var currently_activated : bool = false
+var is_inside_detection_area : bool = false
 
 func get_all_children(node) -> Array:
 	var nodes : Array = []
@@ -23,13 +25,23 @@ func valve_deactivated() -> void:
 func _ready() -> void:
 	return
 
-func _physics_process(delta: float) -> void:
-	return
+func _physics_process(_delta: float) -> void:
+	if is_inside_detection_area and Input.is_action_just_pressed("interact"):
+		if !currently_activated:
+			var tween = $Valve_sprite.create_tween()
+			tween.tween_property($Valve_sprite,"rotation",PI,1)
+			valve_activated()
+			currently_activated = true
+		else:
+			var tween = $Valve_sprite.create_tween()
+			tween.tween_property($Valve_sprite,"rotation",0,1)
+			valve_deactivated()
+			currently_activated = false
 
 
-func _on_area_2d_body_entered(player: Node2D) -> void:
-	valve_activated()
+func _on_area_2d_body_entered(_player: Node2D) -> void:
+	is_inside_detection_area = true
 
 
-func _on_area_2d_body_exited(player: Node2D) -> void:
-	valve_deactivated()
+func _on_area_2d_body_exited(_player: Node2D) -> void:
+	is_inside_detection_area = false
