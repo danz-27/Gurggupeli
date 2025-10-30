03cd306f5d5dbcd104f98f80fe26452bc63ec4c0
KoKonut-7 <kosmo.konttila@gmail.com>
2025-10-31 00:06:49 +0200

    tein kevätsiivoukse, leikin putkien kaa, pelleilin partikkeleilla ja kaikkee muuta randomii

--- gurggupeli/player/water_particle_system.gd
+++ gurggupeli/player/water_particle_system.gd
@@ -1,18 +1,15 @@
 extends GPUParticles2D
 
-@onready var water_particle_system := $WaterParticleSystem
 var was_in_water_last_frame := false
 
 func _physics_process(_delta: float) -> void:
 	
-	position = Player.instance.position
+	position = Player.instance.position + Vector2(0, 2)
 	#check if particles should be emitted
 	if !was_in_water_last_frame and Player.instance.is_in_water():
 		play_water_enter_particles()
 	elif was_in_water_last_frame and !Player.instance.is_in_water():
 		play_water_exit_particles()
-	else:
-		stop_water_particles()
 		
 		#set variables for next frame
 	if Player.instance.is_in_water():
@@ -22,16 +19,21 @@ func _physics_process(_delta: float) -> void:
 
 func play_water_enter_particles() -> void:
 	get_process_material().set_direction(Vector3(-Player.instance.velocity.x,-Player.instance.velocity.y,0))
-	get_process_material().set_param_max(0, 20 + Player.instance.velocity.length())
-	get_process_material().set_param_min(0, 20 + Player.instance.velocity.length() - 10.0)
+	get_process_material().set_param_max(0, 35 + 60 * Player.instance.velocity.length()/250)
+	get_process_material().set_param_min(0, 25 + 20 * Player.instance.velocity.length()/250)
 	emitting = true
+	await get_tree().physics_frame
+	await get_tree().physics_frame
+	await get_tree().physics_frame
+	emitting = false
 	
 func play_water_exit_particles() -> void:
 	get_process_material().set_direction(Vector3(Player.instance.velocity.x,Player.instance.velocity.y,0))
-	get_process_material().set_param_max(0, 20 + Player.instance.velocity.length())
-	get_process_material().set_param_min(0, 20 + Player.instance.velocity.length() - 10.0)
+	get_process_material().set_param_max(0, 35 + 60 * Player.instance.velocity.length()/250)
+	get_process_material().set_param_min(0, 25 + 20 * Player.instance.velocity.length()/250)
 	emitting = true
-
-func stop_water_particles() -> void:
+	await get_tree().physics_frame
+	await get_tree().physics_frame
+	await get_tree().physics_frame
 	emitting = false
 	
