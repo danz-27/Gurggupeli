a36c0dc412d7a72ad2c29d55d9b1b80bc2161437
Kosmo <kosmo.konttila@gmail.com>
2025-11-04 17:12:30 +0200

    dashi partikkelit feidaa nyt ja ground partikkelei tulee animaation mukaan

--- gurggupeli/ground_particles.gd
+++ gurggupeli/ground_particles.gd
@@ -2,7 +2,7 @@ extends GPUParticles2D
 
 func _physics_process(_delta: float) -> void:
 	global_position = Player.instance.global_position + Vector2(0, 7.5)
-	if Player.instance.is_on_floor() and Player.instance.velocity.length() > 25:
+	if Player.instance.is_on_floor() and Player.instance.velocity.length() > 25 and (Player.instance.gurggu.frame < 19 or Player.instance.gurggu.frame > 22):
 		emitting = true
 	else:
 		emitting = false
