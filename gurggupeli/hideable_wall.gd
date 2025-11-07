6ace6a2cc99ad2b57c55826d10f9aaf32f76c4e4
Kosmo <kosmo.konttila@gmail.com>
2025-11-07 09:17:23 +0200

    nyt toi on aika hyvä toi piilotettava seinä

--- gurggupeli/hideable_wall.gd
+++ gurggupeli/hideable_wall.gd
@@ -2,11 +2,15 @@ extends Node2D
 
 var is_hidden: bool = false
 var tile_positions: Array = []
-
-func _activate() -> void:
-	is_hidden = true
 		
 func _physics_process(_delta: float) -> void:
 	if is_hidden:
 		if modulate.a > 0:
 			modulate.a -= 0.01
+	else:
+		if modulate.a < 1:
+			modulate.a += 0.01
+	if $Area2D.has_overlapping_bodies():
+		is_hidden = true
+	else:
+		is_hidden = false
