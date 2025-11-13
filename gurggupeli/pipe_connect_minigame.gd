657b7bdb1b4153c85ba909aa6d2e92c4209b9a6a
Kosmo <kosmo.konttila@gmail.com>
2025-11-13 21:56:20 +0200

    MÄ OON LEGIT MEGAMIND

--- gurggupeli/pipe_connect_minigame.gd
+++ gurggupeli/pipe_connect_minigame.gd
@@ -6,7 +6,10 @@ var shapes: Array = ["straight", "left_turn", "right_turn"]
 var pipes: Array = []
 var won: bool = false
 
+@onready var path: Path2D = $pipe/Path2D
 @onready var texture: Sprite2D = $"hieno onnittelu"
+@onready var head1: Area2D = $pipe/Head1
+@onready var head2: Area2D = $pipe/Head2
 
 var move_position_to_direction: Dictionary = {
 	0 : [0, -1],
@@ -16,9 +19,12 @@ var move_position_to_direction: Dictionary = {
 }
 
 func check_for_win(starting_position: Array, ending_position: Array) -> bool:
+	path.curve.clear_points()
 	var direction_coming_from: int = 1
 	var position_trying_to_go_to_x: int = starting_position[0]
 	var position_trying_to_go_to_y: int = starting_position[1]
+	path.curve.add_point(Vector2(head1.position))
+	path.curve.add_point(Vector2(position_trying_to_go_to_x*24, position_trying_to_go_to_y*24))
 	var pipe_trying_to_go_to: Node = pipes[position_trying_to_go_to_x * 5 + position_trying_to_go_to_y]
 	var iterations: int = 0
 	var maximum_steps: int = pieces_in_x_direction * pieces_in_y_direction
@@ -36,11 +42,13 @@ func check_for_win(starting_position: Array, ending_position: Array) -> bool:
 				#print("direction coming from: ", direction_coming_from)
 				position_trying_to_go_to_x += move_position_to_direction[pipe_trying_to_go_to.class_pipe_instance.get_pipe_hole_directions()[iterations]][0]
 				position_trying_to_go_to_y += move_position_to_direction[pipe_trying_to_go_to.class_pipe_instance.get_pipe_hole_directions()[iterations]][1]
+				path.curve.add_point(Vector2(position_trying_to_go_to_x*24, position_trying_to_go_to_y*24))
 				#print("position after trying to move: ", position_trying_to_go_to_x, position_trying_to_go_to_y)
 				#print("Current pipe trying to get to: ", position_trying_to_go_to_x * 5 + position_trying_to_go_to_y)
 				if position_trying_to_go_to_x <= pieces_in_x_direction - 1 and position_trying_to_go_to_x >= 0 and position_trying_to_go_to_y >= 0 and position_trying_to_go_to_y <= pieces_in_y_direction - 1:
 					pipe_trying_to_go_to = pipes[position_trying_to_go_to_x * 5 + position_trying_to_go_to_y]
 				if position_trying_to_go_to_x == ending_position[0] and position_trying_to_go_to_y == ending_position[1]:
+					path.curve.add_point(Vector2(head2.position))
 					print("connected!")
 					return true
 				break
@@ -65,5 +73,9 @@ func _ready() -> void:
 func _physics_process(_delta: float) -> void:
 	if won:
 		texture.visible = true
+		head1.monitoring = true
+		head2.monitoring = true
 	else:
 		texture.visible = false
+		head1.monitoring = false
+		head2.monitoring = false
