a8c9f4658af5edc3cede96e79c751878b44810ba
Kosmo <kosmo.konttila@gmail.com>
2025-11-14 17:09:35 +0200

    pathfinding putkilo minipeliin lmao

--- gurggupeli/pipe_connect_minigame.gd
+++ gurggupeli/pipe_connect_minigame.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-var pieces_in_x_direction: int = 5
+var pieces_in_x_direction: int = 10
 var pieces_in_y_direction: int = 5
-var shapes: Array = ["straight", "left_turn", "right_turn"]
 var pipes: Array = []
 var won: bool = false
+var locked: bool = false
 
 @onready var path: Path2D = $pipe/Path2D
 @onready var texture: Sprite2D = $"hieno onnittelu"
@@ -18,60 +18,166 @@ var move_position_to_direction: Dictionary = {
 	3 : [-1, 0]
 }
 
-func check_for_win(starting_position: Array, ending_position: Array) -> bool:
+class agent:
+	var positionX: int
+	var positionY: int
+	var direction_going_to: int
+	var max_lifetime: int
+	var positions_path: Array
+	var positions_path_in_pipe_numbers: Array
+	
+	var move_position_to_direction: Dictionary = {
+	0 : [0, -1],
+	1 : [1, 0],
+	2 : [0, 1],
+	3 : [-1, 0]}
+	
+	func move() -> void:
+		positionX += move_position_to_direction[direction_going_to][0]
+		positionY += move_position_to_direction[direction_going_to][1]
+		max_lifetime -= 1
+	
+func calculate_if_possible(starting_position: Array[int], ending_position: Array[int], direction_coming_from: int) -> bool:
+	var agents: Array[agent]
+	var winning_positions_path: Array
+	var has_intersection: bool = false
+	var times_run: int = 0
+	var maximum_times_to_run: int = 200
+	agents.append(agent.new())
+	agents[0].direction_going_to = direction_coming_from
+	agents[0].positionX = starting_position[0]
+	agents[0].positionY = starting_position[1]
+	agents[0].max_lifetime = pieces_in_x_direction * pieces_in_y_direction
+	#print("starting position: ", agents[0].positionX * pieces_in_y_direction + agents[0].positionY)
+	var reached_end: bool = false
+	while !reached_end and agents.size() > 0 and times_run < maximum_times_to_run:
+		for A in agents:
+			#print("Going to direction ", A.direction_going_to, " From position ", A.positionX * pieces_in_y_direction + A.positionY)
+			A.move()
+			#print("moved to: ", A.positionX * pieces_in_y_direction + A.positionY)
+			if A.positionX == ending_position[0] and A.positionY == ending_position[1]:
+				reached_end = true
+				winning_positions_path = A.positions_path_in_pipe_numbers
+			if A.positionX > pieces_in_x_direction - 1 or A.positionX < 0 or A.positionY > pieces_in_y_direction - 1 or A.positionY < 0:
+				agents.erase(A)
+				A = null
+				#print("went out of bounds")
+				break
+			if [A.positionX, A.positionY] not in A.positions_path:
+				A.positions_path.append([A.positionX, A.positionY])
+				A.positions_path_in_pipe_numbers.append(A.positionX * pieces_in_y_direction + A.positionY)
+			elif pipes[A.positionX * pieces_in_y_direction + A.positionY].shape != "4-way_cross":
+				has_intersection = true
+			else:
+				#print("Was already in this tile")
+				agents.erase(A)
+				A = null
+				break
+			if A.max_lifetime <= 0:
+				agents.erase(A)
+				A = null
+				break
+			pipes[A.positionX * pieces_in_y_direction + A.positionY].has_agent = true
+			if pipes[A.positionX * pieces_in_y_direction + A.positionY].shape == "turn":
+				#print(A.positionX * pieces_in_y_direction + A.positionY, " is a turn")
+				var new_agent1: agent = agent.new()
+				var new_agent2: agent = agent.new()
+				new_agent1.direction_going_to = (A.direction_going_to + 1) % 4
+				new_agent1.positionX = A.positionX
+				new_agent1.positionY = A.positionY
+				new_agent1.max_lifetime = A.max_lifetime
+				new_agent1.positions_path = A.positions_path
+				new_agent1.positions_path_in_pipe_numbers = A.positions_path_in_pipe_numbers
+				new_agent2.direction_going_to = (A.direction_going_to + 3) % 4
+				new_agent2.positionX = A.positionX
+				new_agent2.positionY = A.positionY
+				new_agent2.max_lifetime = A.max_lifetime
+				new_agent2.positions_path = A.positions_path
+				new_agent2.positions_path_in_pipe_numbers = A.positions_path_in_pipe_numbers
+				agents.append(new_agent1)
+				agents.append(new_agent2)
+				agents.erase(A)
+				A = null
+				break
+			await get_tree().physics_frame
+			times_run += 1
+	if reached_end:
+		for pipe_instance: pipe in pipes:
+			pipe_instance.has_agent = false
+			if pipe_instance.position_in_array in winning_positions_path:
+				pipe_instance.has_agent = true
+		print(winning_positions_path)
+		return true
+	print(times_run)
+	return false
+
+func check_for_win(starting_position: Array, ending_position: Array, direction_coming_from: int) -> bool:
+	var maximum_steps: int = pieces_in_x_direction * pieces_in_y_direction
 	path.curve.clear_points()
-	var direction_coming_from: int = 1
-	var position_trying_to_go_to_x: int = starting_position[0]
-	var position_trying_to_go_to_y: int = starting_position[1]
+	var position_trying_to_go_to_x: int = starting_position[0] + move_position_to_direction[direction_coming_from][0]
+	var position_trying_to_go_to_y: int = starting_position[1] + move_position_to_direction[direction_coming_from][1]
 	path.curve.add_point(Vector2(head1.position))
 	path.curve.add_point(Vector2(position_trying_to_go_to_x*24, position_trying_to_go_to_y*24))
-	var pipe_trying_to_go_to: Node = pipes[position_trying_to_go_to_x * 5 + position_trying_to_go_to_y]
+	var pipe_trying_to_go_to: Node = pipes[position_trying_to_go_to_x * pieces_in_y_direction + position_trying_to_go_to_y]
 	var iterations: int = 0
-	var maximum_steps: int = pieces_in_x_direction * pieces_in_y_direction
 	for _steps in maximum_steps:
-		iterations = 1
-		for directions: int in pipe_trying_to_go_to.class_pipe_instance.get_pipe_hole_directions():
+		iterations = 5
+		for directions: int in pipe_trying_to_go_to.get_pipe_hole_directions():
 			if direction_coming_from == (directions + 2) % 4:
 				#print("pipe before moving: ", position_trying_to_go_to_x * 5 + position_trying_to_go_to_y)
-				#print("directions: ", pipe_trying_to_go_to.class_pipe_instance.get_pipe_hole_directions())
-				#print("trying to move to direction: ", pipe_trying_to_go_to.class_pipe_instance.get_pipe_hole_directions()[iterations])
-				#print("trying to move to position x: ", move_position_to_direction[pipe_trying_to_go_to.class_pipe_instance.get_pipe_hole_directions()[iterations]][0])
-				#print("trying to move to position y: ", move_position_to_direction[pipe_trying_to_go_to.class_pipe_instance.get_pipe_hole_directions()[iterations]][1])
+				#print("directions: ", pipe_trying_to_go_to.get_pipe_hole_directions())
+				#print("trying to move to direction: ", pipe_trying_to_go_to.get_pipe_hole_directions()[iterations % 4])
+				#print("trying to move to position x: ", move_position_to_direction[pipe_trying_to_go_to.get_pipe_hole_directions()[iterations % 4]][0])
+				#print("trying to move to position y: ", move_position_to_direction[pipe_trying_to_go_to.get_pipe_hole_directions()[iterations % 4]][1])
 				#print("position before trying to move: ",position_trying_to_go_to_x, position_trying_to_go_to_y)
-				direction_coming_from = pipe_trying_to_go_to.class_pipe_instance.get_pipe_hole_directions()[iterations]
+				direction_coming_from = pipe_trying_to_go_to.get_pipe_hole_directions()[iterations % 4]
 				#print("direction coming from: ", direction_coming_from)
-				position_trying_to_go_to_x += move_position_to_direction[pipe_trying_to_go_to.class_pipe_instance.get_pipe_hole_directions()[iterations]][0]
-				position_trying_to_go_to_y += move_position_to_direction[pipe_trying_to_go_to.class_pipe_instance.get_pipe_hole_directions()[iterations]][1]
+				position_trying_to_go_to_x += move_position_to_direction[pipe_trying_to_go_to.get_pipe_hole_directions()[iterations % 4]][0]
+				position_trying_to_go_to_y += move_position_to_direction[pipe_trying_to_go_to.get_pipe_hole_directions()[iterations % 4]][1]
 				path.curve.add_point(Vector2(position_trying_to_go_to_x*24, position_trying_to_go_to_y*24))
 				#print("position after trying to move: ", position_trying_to_go_to_x, position_trying_to_go_to_y)
 				#print("Current pipe trying to get to: ", position_trying_to_go_to_x * 5 + position_trying_to_go_to_y)
 				if position_trying_to_go_to_x <= pieces_in_x_direction - 1 and position_trying_to_go_to_x >= 0 and position_trying_to_go_to_y >= 0 and position_trying_to_go_to_y <= pieces_in_y_direction - 1:
-					pipe_trying_to_go_to = pipes[position_trying_to_go_to_x * 5 + position_trying_to_go_to_y]
+					pipe_trying_to_go_to = pipes[position_trying_to_go_to_x * pieces_in_y_direction + position_trying_to_go_to_y]
 				if position_trying_to_go_to_x == ending_position[0] and position_trying_to_go_to_y == ending_position[1]:
 					path.curve.add_point(Vector2(head2.position))
 					print("connected!")
 					return true
 				break
-			iterations = 0
-	print(position_trying_to_go_to_x, position_trying_to_go_to_y)
+			iterations -= 1
+			
+	#print(position_trying_to_go_to_x, position_trying_to_go_to_y)
 	if position_trying_to_go_to_x == ending_position[0] and position_trying_to_go_to_y == ending_position[1]:
 		print("connected!")
 		return true
 	else:
 		return false
 
-func _ready() -> void:
+func create_tiles() -> void:
 	for i in range(0, pieces_in_x_direction):
 		for j in range(0, pieces_in_y_direction):
-			var pipe_piece: Node = preload("res://pipe_minigame_pipe.tscn").instantiate()
-			add_child.call_deferred(pipe_piece)
+			var pipe_piece: pipe = preload("res://pipe_minigame_pipe.tscn").instantiate()
+			add_child(pipe_piece)
 			pipe_piece.global_position = position
 			pipe_piece.position.x = i * 24
 			pipe_piece.position.y = j * 24
 			pipes.append(pipe_piece)
+			pipe_piece.update_position_in_array()
+	if await calculate_if_possible([-1, 2], [10, 2], 1):
+		print("possible")
+	else:
+		print("busted")
+		for pipe_instance: pipe in pipes:
+			pipe_instance.queue_free()
+		pipes.clear()
+		create_tiles()
+
+func _ready() -> void:
+	create_tiles()
 
 func _physics_process(_delta: float) -> void:
 	if won:
+		locked = true
 		texture.visible = true
 		head1.monitoring = true
 		head2.monitoring = true
--- gurggupeli/pipe_minigame_pipe.gd
+++ gurggupeli/pipe_minigame_pipe.gd
@@ -1,40 +1,57 @@
 extends Node2D
-	
-class pipe:
-	var shapes: Array = ["straight", "left_turn", "right_turn"]
-	var shape: String = shapes.pick_random() #can be "straight", "left_turn", "right_turn"
-	var direction: int = randi_range(0,3)
-	
-	func get_pipe_hole_directions() -> Array:
-		if shape == "straight":
-			return [direction, (direction+2) % 4]
-		elif shape == "left_turn":
-			return [direction, (direction+1) % 4]
-		elif shape == "right_turn":
-			return [direction, (direction+3) % 4]
-		else:
-			return []
+class_name pipe
 
-var class_pipe_instance: pipe = pipe.new()
+static var class_pipe_instance : pipe
+
+var starting_position: Array = [-1, 2]
+var ending_position: Array = [10, 2]
+
+var shapes: Array = ["straight", "turn", "4-way_cross"]
+var shape: String = shapes.pick_random()
+var direction: int = randi_range(0,3)
+
+@onready var agent_texture: Sprite2D = $agent
+var has_agent: bool = false
+
+func get_pipe_hole_directions() -> Array:
+	if shape == "straight":
+		return [direction, (direction+2) % 4]
+	elif shape == "turn":
+		return [direction, (direction+1) % 4]
+	elif shape == "4-way_cross":
+		return [direction, (direction+2) % 4, (direction+1) % 4, (direction+3) % 4]
+	else:
+		return []
+
 var position_in_array: int
 
-func set_frame(shape: String) -> void:
+func update_position_in_array() -> void:
+	position_in_array = get_parent().pipes.find(self)
+
+func set_frame(own_shape: String) -> void:
 	var frames: Dictionary = {
 	"straight" : 2,
-	"left_turn" : 1,
-	"right_turn" : 0}
-	$Sprite2D.frame = frames[shape]
+	"turn" : 1,
+	"4-way_cross" : 5}
+	$Sprite2D.frame = frames[own_shape]
 
 func _ready() -> void:
-	set_frame(class_pipe_instance.shape)
-	$Sprite2D.rotation = class_pipe_instance.direction * PI/2 + PI
+	set_frame(shape)
+	$Sprite2D.rotation = direction * PI/2 + PI
 
 
 func _on_button_pressed() -> void:
-	get_parent().pipes[get_parent().pipes.find(self)].class_pipe_instance.direction = (class_pipe_instance.direction+1) % 4
-	if get_parent().check_for_win([0, 2], [5, 2]):
-		get_parent().won = true
+	if !get_parent().locked:
+		get_parent().pipes[get_parent().pipes.find(self)].direction = (direction+1) % 4
+		if get_parent().check_for_win(starting_position, ending_position, 1):
+			get_parent().won = true
+		else:
+			get_parent().won = false
+		position_in_array = get_parent().pipes.find(self)
+		$Sprite2D.rotation = direction * PI/2 + PI
+
+func _physics_process(_delta: float) -> void:
+	if has_agent:
+		agent_texture.visible = true
 	else:
-		get_parent().won = false
-	position_in_array = get_parent().pipes.find(self)
-	$Sprite2D.rotation = class_pipe_instance.direction * PI/2 + PI
+		agent_texture.visible = false
