fbfb4667e07fe16587efc50521bfa477d0b5e1b8
KoKonut-7 <kosmo.konttila@gmail.com>
2026-01-09 01:01:15 +0200

    piilotin viimesen vialin ja lisäsin paljon ohjaavia graffiteja

--- gurggupeli/GUI/inventory/vile.gd
+++ gurggupeli/GUI/inventory/vile.gd
@@ -6,6 +6,10 @@ extends Node2D
 func _ready() -> void:
 	if GlobalVariables.collected_viles.has(vile_tag):
 		sprite.visible = false
+	var tween: Tween = get_tree().create_tween()
+	tween.set_loops(9999)
+	tween.tween_property(sprite, "position", Vector2(0, -2), 3).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(sprite, "position", Vector2(0, 2), 3).set_ease(Tween.EASE_IN_OUT)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
